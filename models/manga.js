a46b0161bc2b02518f4a1ff28bce222c93f39406
Cody Dawkins <dawkinscody4@gmail.com>
2023-01-25 21:25:40 -0500

    fixed handlebar formatting

--- models/manga.js
+++ models/manga.js
@@ -6,20 +6,7 @@ class Manga extends Model {}
 
 
 Manga.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
+  //
 },
     
 {
