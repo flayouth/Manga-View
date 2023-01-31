deef0a6550c365c7f5f5bf70917bfbb3ab55f89d
Cody Dawkins <dawkinscody4@gmail.com>
2023-01-31 16:14:09 -0500

    Heroku Deployment Fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const sequelize = require('./config/connection');
 
 var app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.locals.pluralize = require('pluralize');
 
